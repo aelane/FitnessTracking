d3c81b520b2ebddf98c635dadaf22af28273f699
aelane <aelane@berkeley.edu>
2018-07-15 10:14:33 -0500

    Data Science Cookiecutter folder structure

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.0.1',
+    description='EDA and ML experiments & application to Activity & Fitness tracking data sets',
+    author='Annie Lane',
+    license='',
+)
